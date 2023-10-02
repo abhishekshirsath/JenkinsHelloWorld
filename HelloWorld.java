e1717a51edce175626e48714e6e770360ff24aba
Abhishek Shirsath <93754082+abhishekshirsath@users.noreply.github.com>
2023-10-02 19:56:32 +0530

    Update HelloWorld.java
    using webhook

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld{
 public static void main(String args[]){
-System.out.println("Hello Jenkins tutorials after third commit");
+System.out.println("Hello Jenkins tutorials trying webhooks");
 }
 }
